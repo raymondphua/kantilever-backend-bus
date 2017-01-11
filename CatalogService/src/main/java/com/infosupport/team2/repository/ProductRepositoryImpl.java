eadea836c918b29df6cf8712f42e1ba058f1ed8c
tgixxer <robin_vj@hotmail.com>
2017-01-11 12:20:22 +0100

    Merge branches 'dev-master' and 'rest-catalog' of http://10.32.43.248/stash/scm/cm/kantilever-team2-backend-bus into rest-catalog Added filter in repository to filter data based on parameters of the api call.

--- CatalogService/src/main/java/com/infosupport/team2/repository/ProductRepositoryImpl.java
+++ CatalogService/src/main/java/com/infosupport/team2/repository/ProductRepositoryImpl.java
@@ -1,24 +1,62 @@
-//package com.infosupport.team2.repository;
-//
-//import com.infosupport.team2.model.Product;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-///**
-// * Created by Robin on 11-1-2017.
-// */
-//public class ProductRepositoryImpl implements ProductRepository {
-//
-//    @Autowired
-//    MongoTemplate mongoTemplate;
-//
-//
-//    @Override
-//    public List<Product> filterProducts(Map<String, String> filters)
-//    {
-//
-//    }
-//}
+package com.infosupport.team2.repository;
+
+import com.infosupport.team2.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Robin on 11-1-2017.
+ */
+public class ProductRepositoryImpl implements CustomProductRepository {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Override
+    public List<Product> filterProducts(Map<String, String> filters) {
+        Query productFilter = new Query();
+
+        filters.forEach((k, v) -> {
+            String property = getPropertyForParam(k);
+            if (property != null) {
+                productFilter.addCriteria(getCriteriaForParam(property, v.split(",")));
+            }
+        });
+
+        return mongoTemplate.find(productFilter, Product.class);
+    }
+
+    @Override
+    public List<String> getAllCategories() {
+        return mongoTemplate.getCollection("products").distinct("categories._id");
+    }
+
+    @Override
+    public List<String> getAllBrands() {
+        return mongoTemplate.getCollection("products").distinct("brand._id");
+    }
+
+    private Criteria getCriteriaForParam(String property, String[] values) {
+        return Criteria.where(property).in(Arrays.asList(values));
+    }
+
+    private String getPropertyForParam(String param) {
+        switch (param.toLowerCase()) {
+            case "brands":
+            case "brand":
+                return "brand._id";
+            case "categories":
+            case "category":
+            case "categorie":
+                return "categories._id";
+        }
+
+        return null;
+    }
+}
--- CatalogService/src/main/java/com/infosupport/team2/resource/ProductResource.java
+++ CatalogService/src/main/java/com/infosupport/team2/resource/ProductResource.java
@@ -4,10 +4,7 @@ import com.infosupport.team2.model.Product;
 import com.infosupport.team2.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -15,14 +12,27 @@ import java.util.Map;
 /**
  * Created by Robin on 10-1-2017.
  */
+@CrossOrigin
 @RestController
 public class ProductResource {
     @Autowired
     private ProductRepository productRepo;
 
-    @RequestMapping(value = {"/product/", "/product"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/products/", "/products"}, method = RequestMethod.GET)
     public List<Product> allProducts(@RequestParam Map<String,String> allRequestParams, ModelMap model){
-        return productRepo.findAll();
+        return productRepo.filterProducts(allRequestParams);
+    }
+
+    @RequestMapping(value = {"/categories/", "/categories"}, method = RequestMethod.GET)
+    public List<String> allCategories()
+    {
+        return productRepo.getAllCategories();
+    }
+
+    @RequestMapping(value = {"/brands/", "/brands"}, method = RequestMethod.GET)
+    public List<String> allBrands()
+    {
+        return productRepo.getAllBrands();
     }
 
 }
